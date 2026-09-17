52fdcec9cc8d33161f247ebb61fc7fd37afda28d
szfg2 <161961456+szfg2@users.noreply.github.com>
2026-09-17 18:29:38 +0800

    Create keyvault.data.js

---
+++ keyvault.data.js
@@ -0,0 +1,26 @@
+/* Locked keys for keyvault.js, made by keyvault-setup.html on 2026-09-17.
+ * Encrypted: unreadable without the code, which is never stored here. */
+window.KEY_VAULT = {
+  "v": 1,
+  "kdf": "PBKDF2-SHA256",
+  "iterations": 2000000,
+  "created": "2026-09-17",
+  "names": [
+    "cdg_api_key",
+    "anthropic_api_key",
+    "cdg_deepseek_key",
+    "council_gemini_key",
+    "dictation_openai_key",
+    "cdg_provider",
+    "dictation_gh_token",
+    "dictation_gh_owner",
+    "dictation_gh_repo",
+    "dictation_gh_branch",
+    "resblue_gh_path",
+    "resclinic_gh_path",
+    "iculist_gh_path"
+  ],
+  "salt": "kHpoUjlLc1HHPR/fWyaJ3Q==",
+  "iv": "3yDozmuvXQx9F3B5",
+  "data": "ptBUR43Z/c+xgTR0rVz//g4+tucqXLlHiVM6ZaJyGjjkYPgaUPkSnd9QI7Ne1j+LnCUz0YrthRc0FGVcmxkO/axcWP/vFn8jI9cFU5ZWQW8RA5fkQZbfTDJ/h63h8Ebka/sU2ZWUik2eGxY/socdfE+Pf9IdU5HVcVzzRwYRrDY4la0Y+kwc5CX2v4DAlSIe2WmyybrQK1Nvo8Iv2MlyW0Szlv7dmxpku6XQZLb8bK06Rn9+e+IluEymWtHKhadlKsHgRF6zDBL2KrxGGBmbLEsGTpnMtk54LysBRKR8gRYeW9GlGGXJmDMYWQmw/32g5vp0IjopPhA2r2WuRRQip5pEHcydV2NU1J3wb8yUh9Np8i0VAxLa2NFLUHRCjKnhohbL/SGlHidrokk/HfWDmoPIi7NsNk1QopRE6cuXlK678NAcqlF6ZrqD4jhy6ygSO7QQg2KAy0a7cPLZHQ+Dku36v/tRnJG09jYoyKamu3e/q8scPuij9MG94XEdfIG1ZUeSzsqqir+QJUAGQ4QuSL2gILcHVUaUI+hUXkZjdSBVKfAO7TbuZFExNdW3Jjl8bUmivL5omW/XHnOX3duvi7AI2AImJoZ4NhUXPTQKpC53zXilOe717oC6c3oel6QKAXNZJw6gkpYYW7asyQ6yUlFWMP85boTYSTatNaq8PEMjor1qWMPQy9Q6o7edHepzxLyURtF58t6lk7u3SAJSSizp3EX5XTjphkU8ybrEvImjU0YJDZFM/dmHxMeDRFMT9oLeKRuz6mHZ1+WCqMQnjrrk+M25B7ZLXcLtJ43Y8Zku3JtVI2XtvJ2YAs7bKq0upqkwUM8uoof/DBOAHp6F9RO5fYeaDNqo5XGiVn2HY5M9CijmkWQ/N8bw2VSzKgbOflcZwf7PMlJBqOxZ351eD5kfBX582I6mPLS29+6z/21WsbjG91nHMnO9IK6Vb1fmGk/1W+i2OCl+oHPogXYN3+mbsJRk75+b2oOVRCcgE86CB4J27vi7LwIm1v3tKByOasfslT6/H8Dn5jt0DDteH48YmgTiw946TfYg4JP+NQl2TmKUvz5nS7R/ho5AAw0LuIpaQAuCgENvCU+88RB0VJoGoAN4GBgWUucfoLLd+D4yjurShksW3CosyHmhPKjDG98cNbh0Dv8HVkEi+4ZECtiGHsckhiQPUb1/sygN0NmoHVEvqThtnz6rF1HdiFaN1UM6wg9T2yic49YecXiKMVL11OpA4yzhCiHew63qdk4G0M4fXN6AffYhnFYHXo0/Ah3soA=="
+};
